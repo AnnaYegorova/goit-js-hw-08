838d16657f7fc5b2c27b21f7bd01ee8376ca7b3c
AnnaYegorova <good4anna@gmail.com>
2021-12-25 21:08:23 +0200

    fix task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,26 @@
 import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 const STORAGE_KEY = 'videoplayer-current-time';
-player.on('play', function () {});
-player.on('timeupdate', function (duration) {
-  const currentTime = localStorage.setItem(STORAGE_KEY, JSON.stringify(duration));
-  throttle(currentTime, 1000);
-  console.log('currentTime', JSON.parse(currentTime));
-});
 
-const secondsFromLocaleStorage = JSON.parse(localStorage.getItem(STORAGE_KEY)).seconds;
-console.log(secondsFromLocaleStorage);
-player
-  .setCurrentTime(secondsFromLocaleStorage)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+readFromLS();
+player.on('play', onPlay);
+function onPlay() {
+  console.log('play vimeo');
+}
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+function onTimeUpdate(seconds) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(seconds));
+}
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+function readFromLS() {
+  const secondsFromLocaleStorage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log(secondsFromLocaleStorage);
+  if (!secondsFromLocaleStorage) {
+    return;
+  }
+  player.setCurrentTime(secondsFromLocaleStorage.seconds);
+}
